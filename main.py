12f4a6978b4c24c6107cf4db8c4ce333aa4d3576
gbuenos <gabrielhbs@outlook.com.br>
2024-05-08 23:40:39 +0100

    1.1

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import streamlit as st
 
 # Import das páginas
-from pages import eventbrite_dashboard, community_dashboard
 
 # Configuração da barra lateral para seleção de página
-st.sidebar.title('Navigation')
-page = st.sidebar.radio('Go to', ['Eventbrite Dashboard', 'Community Dashboard'])
-
-# Direcionamento para a página selecionada
-if page == 'Eventbrite Dashboard':
-    eventbrite_dashboard.show()
-elif page == 'Community Dashboard':
-    community_dashboard.show()
+st.title('Main')
